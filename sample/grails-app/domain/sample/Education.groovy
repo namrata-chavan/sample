f4f3be54a482d8b4ac45d65a3c3101f6b83e03ac
Namrata-Chavan <namrata.chavan23@gmail.com>
2013-07-03 10:36:21 +0530

    attribute added

--- sample/grails-app/domain/sample/Education.groovy
+++ sample/grails-app/domain/sample/Education.groovy
@@ -3,8 +3,10 @@ package sample
 class Education {
 	Person name
 	String education
+	String branch
     static constraints = {
 		education(blank:false)
 		name(nullable:false)
+		branch(nullable:false)
     }
 }
